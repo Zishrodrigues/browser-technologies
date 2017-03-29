e8c04f5b52ece99a5de585f12155d07b9c0e2809
Zishrodrigues <zish.rodrigues@gmail.com>
2017-03-29 11:05:41 +0200

    filter

---
+++ week-3-demo/static/script.js
@@ -0,0 +1,17 @@
+function filtering() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("listUl");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        var kek = li[i];
+        console.log(kek.innerHTML);
+        if (kek.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
